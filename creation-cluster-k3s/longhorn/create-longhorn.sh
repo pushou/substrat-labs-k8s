9ef5f8e170395db7a4e4e893c4141215aec944f5
Jean-Marc Pouchoulon <jean-marc.pouchoulon@umontpellier.fr>
2020-08-04 18:00:37 +0200

    rajout de metallb et de longhorn lors du build

--- creation-cluster-k3s/longhorn/create-longhorn.sh
+++ creation-cluster-k3s/longhorn/create-longhorn.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 shopt -s expand_aliases
 alias k='kubectl'
-helm install nginx-ingress stable/nginx-ingress --namespace kube-system --set defaultBackend.enabled=false
 k apply -f https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/longhorn.yaml
 USER=longhorn; PASSWORD=longhorn; echo "${USER}:$(openssl passwd -stdin -apr1 <<< ${PASSWORD})" >> auth
 k -n longhorn-system create secret generic basic-auth --from-file=auth
-k apply -f longhorn-ingress.yaml
-k apply -f longhorn-service.yaml
-#k apply -f  longhorn-example-mysql.yaml
+k apply -f ./longhorn-service.yaml
+#k patch svc longhorn-frontend -n longhorn-system --patch "$(cat patch-longfrontend-service.yaml)"
 k get pods --namespace longhorn-system
-k get services  -n kube-system -l app=nginx-ingress -o wide
