5dec9908368eb2df1c81c796db1aab12299d27fc
Jean-Marc Pouchoulon <jean-marc.pouchoulon@umontpellier.fr>
2020-08-05 22:48:48 +0200

    passage web ui de longhorn sur le port 8000 pour éviter la collision avec un ingress

--- creation-cluster-k3s/longhorn/create-longhorn.sh
+++ creation-cluster-k3s/longhorn/create-longhorn.sh
@@ -4,6 +4,7 @@ alias k='kubectl'
 k apply -f https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/longhorn.yaml
 USER=longhorn; PASSWORD=longhorn; echo "${USER}:$(openssl passwd -stdin -apr1 <<< ${PASSWORD})" >> auth
 k -n longhorn-system create secret generic basic-auth --from-file=auth
-k apply -f ./longhorn-service.yaml
+sleep 60
+k apply -f $PWD/longhorn/longhorn-service.yaml
 #k patch svc longhorn-frontend -n longhorn-system --patch "$(cat patch-longfrontend-service.yaml)"
 k get pods --namespace longhorn-system
