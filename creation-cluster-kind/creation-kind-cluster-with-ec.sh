70fec6f2fa6d50bda2353469b13d6d900f299e8b
automation jmp <jean-marc.pouchoulon@umontpellier.fr>
2021-04-30 23:26:14 +0200

    ajout d'un registry local + upgrade du server de métrique

--- creation-cluster-kind/creation-kind-cluster-with-ec.sh
+++ creation-cluster-kind/creation-kind-cluster-with-ec.sh
@@ -1,6 +1,18 @@
+# create registry container unless it already exists
+reg_name='kind-registry'
+reg_port='5000'
+running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+if [ "${running}" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+    registry:2
+fi
 export KUBECONFIG=""
 kind delete cluster --name tp1k8s
 echo "creation du cluster kind avec la feature-gate ephemeral-container" 
+# connect the registry to the cluster network
+# (the network may already be connected)
+docker network connect "kind" "${reg_name}" || true
 cat <<EOF | kind create cluster --name tp1k8s --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
@@ -36,6 +48,10 @@ kubeadmConfigPatches:
   featureGates:
     EphemeralContainers: true
     CSIInlineVolume: true
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_name}:${reg_port}"]
 nodes:
 - role: control-plane
   kubeadmConfigPatches:
@@ -112,5 +128,19 @@ kind get  kubeconfig --name  tp1k8s > kindkubeconfig
 echo  "installation du serveur de métriques"
 kubectl apply -f components.yaml
 
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
 #echo  "installation open-iscsi"
 #for cont in $(docker ps -q); do docker exec -it $cont  bash -c "apt-get update && apt-get -y install open-iscsi"; done
