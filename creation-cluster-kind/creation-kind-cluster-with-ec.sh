c0c8616f87b8a3aed267a6d3d560ecae31f7c52e
Jean-Marc Pouchoulon <jean-marc.pouchoulon@umontpellier.fr>
2021-06-12 17:08:06 +0200

    ajout de nerdctl dans les containers Docker

--- creation-cluster-kind/creation-kind-cluster-with-ec.sh
+++ creation-cluster-kind/creation-kind-cluster-with-ec.sh
@@ -146,5 +146,10 @@ data:
     host: "localhost:${reg_port}"
     help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
 EOF
+
+echo  "installation de nerdctl"
+
+for cont in $(docker ps |grep -v registry|awk '{print $1}'|grep -v CONTAINER); do docker exec -it $cont  bash -c "apt update && apt -y install wget && cd /tmp && wget https://github.com/containerd/nerdctl/releases/download/v0.8.3/nerdctl-0.8.3-linux-amd64.tar.gz && tar xvfz nerdctl-0.8.3-linux-amd64.tar.gz && mv /tmp/nerdctl /usr/local/bin/nerdctl "; done  
+
 #echo  "installation open-iscsi"
 #for cont in $(docker ps -q); do docker exec -it $cont  bash -c "apt-get update && apt-get -y install open-iscsi"; done
