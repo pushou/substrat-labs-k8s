f0f5b01bf4467ed9fff0a9e490e7d8b7e60ae4d5
pushou <jeanmarc.pouchoulon@gmail.com>
2021-06-16 16:25:40 +0200

    modif bidon pour test de remotevs

--- creation-cluster-kind/creation-kind-cluster-with-ec.sh
+++ creation-cluster-kind/creation-kind-cluster-with-ec.sh
@@ -149,6 +149,7 @@ EOF
 
 echo  "installation de nerdctl"
 
+
 for cont in $(docker ps |grep -v registry|awk '{print $1}'|grep -v CONTAINER); do docker exec -it $cont  bash -c "apt update && apt -y install wget && cd /tmp && wget https://github.com/containerd/nerdctl/releases/download/v0.8.3/nerdctl-0.8.3-linux-amd64.tar.gz && tar xvfz nerdctl-0.8.3-linux-amd64.tar.gz && mv /tmp/nerdctl /usr/local/bin/nerdctl "; done  
 
 export KUBECONFIG=$(pwd)/kubeconfig
